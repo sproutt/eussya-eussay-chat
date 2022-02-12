9a0b2b5f4706b3476877dd228348eda6f52a7ddd
Bellroute <kjkun7631@gmail.com>
2022-02-12 20:55:22 +0900

    init

---
+++ src/main/java/com/sproutt/eussyaeussyachat/EussyaEussyaChatApplication.java
@@ -0,0 +1,13 @@
+package com.sproutt.eussyaeussyachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EussyaEussyaChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EussyaEussyaChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sproutt/eussyaeussyachat/EussyaEussyaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sproutt.eussyaeussyachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EussyaEussyaChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
